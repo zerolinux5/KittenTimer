89d81ceb375256cd64db04b8038025a282d43802
Jesus Magana <maganazero5@gmail.com>
2014-05-28 19:37:35 -0700

    Timer now works correctly

--- src/com/ZeroLinux5/kittentimer/MainActivity.java
+++ src/com/ZeroLinux5/kittentimer/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.CountDownTimer;
 import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	private long counter;
 	private CountDownTimer timer;
+	private int countingDown = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,19 +46,72 @@ public class MainActivity extends Activity {
 	}
 	
 	public void clickStartStop(View v) {
-		
+		    if (timer != null) {
+		        timer.cancel();
+		        countingDown = 0;
+		    }
+		    displayCount(counter);
+		    if (counter > 0) {
+		    	timerSet();
+		    	countingDown = 1;
+		    }
 	}
 	
+	protected void displayCount(long milliseconds) {
+		// variables
+		TextView countString = (TextView) findViewById(R.id.textView1);
+		long seconds = milliseconds / 1000;
+		long minutes = seconds / 60;
+		
+	    seconds = seconds % 60;  
+	    minutes = minutes % 60;  
+	    
+	    String sec = String.valueOf(seconds);  
+	    String min = String.valueOf(minutes);  
+
+	    if (seconds < 10)  
+	        sec = "0" + seconds;  
+	    if (minutes < 10)  
+	        min= "0" + minutes;  
+	    
+		countString.setText(min + " : " + sec);
+	}
+
 	public void clickPlus(View v) {
 		counter += 60 * 1000;
+		if (countingDown == 1){
+			clickStartStop(v);
+		} else {
+			displayCount(counter);
+		}
 	}
 	
 	public void clickMinus(View v) {
 		counter -= 60 * 1000;
 		counter = Math.max(0, counter);		
+		if (countingDown == 1){
+			clickStartStop(v);
+		} else {
+			displayCount(counter);
+		}
 	}
 	
 	public void clickReset(View v) {
 		
 	}
+	
+	private void timerSet(){
+        timer = new CountDownTimer(counter, 1000) {
+            public void onTick(long remainingTimeMillis) {
+                counter = remainingTimeMillis;
+                displayCount(counter);
+            }
+            public void onFinish() {
+                displayCount(0);
+                counter = 0;
+            }
+        };
+    timer.start();
+	}
+
 }
